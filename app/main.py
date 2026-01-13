ac4292f370ea1c65428f4438e15fd04e50a0ecbe
Ikram-sabila <ikramsabila@gmail.com>
2026-01-13 11:09:20 +0700

    Add image URL embedding extraction and update face embedding pipeline logic

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Depends
+from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 from sqlmodel import Session
+from pathlib import Path
 
 from app.routes.face_embedding_routes import router as face_embedding_router
 from app.routes.face_recognition_routes import router as face_recognition_router
@@ -9,11 +11,22 @@ from app.db.session import get_session, create_db_and_tables
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
+# Ini debug
+BASE_DIR = Path(__file__).resolve().parent.parent
+PICTURE_DIR = BASE_DIR / "picture"
+
 app = FastAPI()
 
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
+# Ini debug
+app.mount(
+    "/picture",
+    StaticFiles(directory=PICTURE_DIR),
+    name="picture"
+)
+
+# @app.on_event("startup")
+# def on_startup():
+#     create_db_and_tables()
 
 
 app.include_router(face_embedding_router)
