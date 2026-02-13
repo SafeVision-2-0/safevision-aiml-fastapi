ad4129f699152474a587c5413429af9478b03e42
Halilintar Daiva <daiva@halilintar.my.id>
2026-02-13 11:11:21 +0700

    Revert "fix: automatic timestamp fields in SQLAlchemy models"
    This reverts commit 2933c7b13ede60d325daeba8926788e8d132d83d.

--- app/models/face_embedding_model.py
+++ app/models/face_embedding_model.py
@@ -1,6 +1,5 @@
 from sqlmodel import Field, SQLModel, Relationship
 from sqlalchemy import Column, JSON
-from datetime import datetime
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -13,7 +12,5 @@ class FaceEmbedding(SQLModel, table=True):
     profile_image_id: int = Field(foreign_key="profile_image.id")
     vector_casia: list[float] = Field(sa_column=Column(JSON))
     vector_vgg: list[float] = Field(sa_column=Column(JSON))
-    created_at: datetime = Field(default_factory=datetime.now)
-    updated_at: datetime = Field(default_factory=datetime.now)
 
     profile_image: "ProfileImage" = Relationship(back_populates="embeddings")
